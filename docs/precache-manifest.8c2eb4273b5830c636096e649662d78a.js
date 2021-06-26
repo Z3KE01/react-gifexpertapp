7fa255919ecc84df15406c06abf75ab4abf05b79
Ezequiel Simon <esimon@ies-sabadell.cat>
2021-06-26 21:30:12 +0200

    docs agregado

---
+++ docs/precache-manifest.8c2eb4273b5830c636096e649662d78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ebb85726111b79f5c79df52977e9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927a3eb77c71cfa71b27",
+    "url": "/static/css/main.bbeaa0d8.chunk.css"
+  },
+  {
+    "revision": "e13293c431772057daf3",
+    "url": "/static/js/2.7fae8f10.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.7fae8f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927a3eb77c71cfa71b27",
+    "url": "/static/js/main.630fa34c.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
